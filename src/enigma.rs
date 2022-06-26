d584ff9ebe3f53ed9d08e2094409bd73785b27b3
Brandon Piña <brandonpi265@gmail.com>
2022-06-26 02:05:44 -0500

    It compiles so that's something I guess

---
+++ src/enigma.rs
@@ -0,0 +1,63 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use crate::reflector::Reflector;
+use crate::rotor::Rotor;
+
+pub(crate) struct Enigma {
+    reflector: Reflector,
+    rotors: Vec<Rotor>,
+}
+
+impl Enigma {
+    pub fn new(rotors: Vec<Rotor>, reflector: Reflector) -> Enigma {
+        Enigma { rotors, reflector }
+    }
+
+    pub fn encode(&mut self, message: String) -> String {
+        let cipher_text = message.chars().map(|c| self.encode_char(c)).collect();
+        cipher_text
+    }
+
+    fn encode_char(&mut self, c: char) -> char {
+        self.rotors[0].rotate();
+
+        let mut iterhandle = self.rotors.iter_mut().peekable();
+
+        /*for el in iterhandle {
+            match iterhandle.peek_mut() {
+                Some(next_rotor) => match el.should_advance_next() {
+                    true => next_rotor.rotate(),
+                    false => (),
+                },
+                None => (),
+            }
+        }*/
+
+        while let Some(el) = iterhandle.next(){
+            match iterhandle.peek_mut(){
+                Some(next_rotor)=> match el.should_advance_next(){
+                    true => next_rotor.rotate(),
+                    false => ()
+                },
+                None => ()
+            }
+        }
+
+
+
+        let first_pass = self
+            .rotors
+            .iter()
+            .fold(c, |acc, current_rotor| current_rotor.encode(acc));
+
+        let reflect_pass = self.reflector.encode(first_pass);
+
+        let last_rotor_pass = self
+            .rotors
+            .iter()
+            .fold(reflect_pass, |acc, current_rotor| current_rotor.encode(acc));
+
+        last_rotor_pass
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod rotor;
+mod reflector;
+mod enigma;
+
+use std::env;
+
+use crate::enigma::{Enigma};
+use crate::rotor::{rotors};
+use crate::reflector::{reflectors};
 
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_BACKTRACE","1");
+    let message = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG";
+    let rotor_config = vec![rotors::I,rotors::II,rotors::III];
+
+    let mut enigma_machine = Enigma::new(rotor_config,reflectors::A);
+
+    let cipher_text = enigma_machine.encode(message.to_string());
+    println!("{}",cipher_text);
 }
