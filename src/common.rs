bcb31a002f7e38d8113a360f5333c5a1e2e72b5e
Brandon Piña <brandonpi265@gmail.com>
2022-09-29 21:12:57 -0500

    Implementations for position and character

--- src/common.rs
+++ src/common.rs
@@ -1,14 +1,43 @@
+use std::ops::Add;
+
 #[derive(Debug)]
-pub struct Character(u8);
+pub struct Character(char);
+pub struct Position(u8);
 
 impl TryFrom<char> for Character {
     type Error = &'static str;
 
     fn try_from(value: char) -> Result<Self, Self::Error> {
         let value_uppercase = value.to_ascii_uppercase();
-        match value_uppercase{
-            'A'..='Z' => Ok(Character(value_uppercase as u8 - b'A')),
-            _ => Err("Parsing error: only valid characters are A-Z")
+        match value_uppercase {
+            'A'..='Z' => Ok(Character(value_uppercase)),
+            _ => Err("Parsing error: only valid characters are A-Z"),
         }
+    }
+}
+
+impl TryFrom<u8> for Position {
+    type Error = &'static str;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0..=25 => Ok(Position(value)),
+            _ => Err("Parsing error: only valid positions are 0..=25"),
+        }
+    }
+}
+
+impl Add<u8> for Position {
+    type Output = Position;
+
+    fn add(self, rhs: u8) -> Self::Output {
+        let offset = (self.0 + rhs) % 26;
+        Position(offset)
+    }
+}
+
+impl Position{
+    pub fn advance(mut self){
+        self = self + 1_u8;
     }
 }
