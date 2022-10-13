f2c4bccf5e0e7d8ebc8a4554402745c807ec0111
Brandon Piña <brandonpi265@gmail.com>
2022-10-13 17:26:11 -0500

    Encode strings

--- src/enigma.rs
+++ src/enigma.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 
 use crate::{
     cipher::{Decode, Encode},
-    common::Character,
+    common::{Character, ParsingError},
     plugboard::Plugboard,
     reflector::{Reflector, Reflectors},
     rotor::{Rotor, RotorConfig, Rotors},
@@ -26,13 +26,27 @@ impl Enigma {
         }
     }
 
-    pub fn encode_at(&self, c: Character, n: usize) -> Character {
+    fn encode_at(&self, c: Character, n: usize) -> Character {
         let plugboard_enc = self.plugboard.encode(c);
         let rotor_enc = self.rotors.encode_at(plugboard_enc, n);
         let reflector_enc = self.reflector.encode(rotor_enc);
         let rotor_dec = self.rotors.decode_at(reflector_enc, n);
         let plugboard_dec = self.plugboard.decode(rotor_dec);
         plugboard_dec
+    }
+
+    pub fn encode(&self, s: &String) -> String {
+        s.chars()
+            .map(|c| Character::try_from(c))
+            .enumerate()
+            .map(|(n, c)| match c {
+                Ok(plain) => self.encode_at(plain, n).into(),
+                Err(e) => match e {
+                    ParsingError::Charset(non_alphabetic) => non_alphabetic,
+                    _ => unreachable!(),
+                },
+            })
+            .collect()
     }
 }
 
@@ -47,18 +61,20 @@ mod tests {
 
     use super::Enigma;
 
+    fn construct_enigma() -> Enigma {
+        let rotors: RotorConfig =
+            RotorConfig::try_from([(Rotors::I, 'A'), (Rotors::II, 'X'), (Rotors::IV, 'N')])
+                .unwrap();
+        let plugs = Plugs::try_from(vec![]).unwrap();
+        let plugboard: Plugboard = Plugboard::try_from(plugs).unwrap();
+        let reflector: Reflectors = Reflectors::B;
+
+        Enigma::new(rotors, plugboard, reflector)
+    }
+
     #[test]
     fn codec() {
-        let e = {
-            let rotors: RotorConfig =
-                RotorConfig::try_from([(Rotors::I, 'A'), (Rotors::II, 'X'), (Rotors::IV, 'N')])
-                    .unwrap();
-            let plugs = Plugs::try_from(vec![]).unwrap();
-            let plugboard: Plugboard = Plugboard::try_from(plugs).unwrap();
-            let reflector: Reflectors = Reflectors::B;
-
-            Enigma::new(rotors, plugboard, reflector)
-        };
+        let e = construct_enigma();
         (0..1000).into_iter().for_each(|n| {
             ('A'..='Z')
                 .into_iter()
@@ -69,5 +85,16 @@ mod tests {
                     assert_eq!(pt, c)
                 })
         })
+    }
+
+    #[test]
+    fn codec_string() {
+        let plaintext = String::from("THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG");
+        let e = construct_enigma();
+
+        let ciphertext = e.encode(&plaintext);
+        let result = e.encode(&ciphertext);
+
+        assert_eq!(plaintext, result)
     }
 }
